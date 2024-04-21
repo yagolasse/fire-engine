042fe041ac6fed78c0165b99718772c55b04843b
Yago Lasse <yagolasse@gmail.com>
2024-04-21 19:00:03 -0300

    Add types definitions

---
+++ src/core/defines.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// @brief Unsigned 8-bit integer.
+typedef unsigned char u8;
+// @brief Unsigned 16-bit integer.
+typedef unsigned short int u16;
+// @brief Unsigned 32-bit integer.
+typedef unsigned int u32;
+// @brief Unsigned 64-bit integer.
+typedef unsigned long long u64;
+
+// @brief Signed 8-bit integer.
+typedef signed char i8;
+// @brief Signed 16-bit integer.
+typedef signed short int i16;
+// @brief Signed 32-bit integer.
+typedef signed int i32;
+// @brief Signed 64-bit integer.
+typedef signed long long i64;
+
+// @brief 32-bit precision floating point number.
+typedef float f32;
+// @brief 64-bit precision floating point number.
+typedef double f64;
+
+// @brief 8-bit boolean.
+typedef bool b8;
+
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DEFINE_MATH_OPERATORS
+#endif
